2d51c82644b925304e33b5a1edcce9dc39a4ac8c
Daniel Krolopp <dkrolopp@purdue.edu>
2018-05-17 12:03:11 -0500

    Imported libs

--- col.py
+++ col.py
@@ -1,5 +1,10 @@
 __author__ = 'Daniel Krolopp'
 __version__ = '1.0'
 
+import sys
+
+sys.path.insert(0, 'lib/wolfram/Python_Binding_1_1')
+import wap
+
 jobName = raw_input("What type of job are you looking for? ")
 jobLocationName = raw_input("Where would you like to work? ")
